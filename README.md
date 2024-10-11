421c121f160e637fde1c8891e0c6cf4f39e0704f
Guillermo German Speicher <guillermo.speicher@gmail.com>
2024-10-11 18:13:43 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ort-be-tp3-grupo5-parcial
+Repositorio del parcial de Taller de Programación 3, de la ORT, Sede Belgrano, Grupo 5, 2do cuatrimestre del año 2024
---
+++ app/src/main/java/com/example/ortbetp3grupo5parcial/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ortbetp3grupo5parcial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ortbetp3grupo5parcial.ui.theme.OrtBeTp3Grupo5ParcialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            OrtBeTp3Grupo5ParcialTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    OrtBeTp3Grupo5ParcialTheme {
+        Greeting("Android")
+    }
+}
