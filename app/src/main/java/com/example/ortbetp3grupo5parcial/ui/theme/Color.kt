0ee40aaf5088b733b8b0229ba7deaa23ebef9295
Guillermo German Speicher <guillermo.speicher@gmail.com>
2024-10-13 16:03:29 -0300

    feat(QuantityButton, Colors, Resources): Icons added,QuantityButton generated, Colores added #23

--- app/src/main/java/com/example/ortbetp3grupo5parcial/ui/theme/Color.kt
+++ app/src/main/java/com/example/ortbetp3grupo5parcial/ui/theme/Color.kt
@@ -6,6 +6,9 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
+val Green40 = Color (0xFF53B175)
+val Pink40 = Color(0xFF7D5260)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+val Gray20 = Color(0xFFE2E2E2)
