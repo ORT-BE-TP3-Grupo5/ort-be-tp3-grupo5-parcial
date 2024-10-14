80570ef0b5ce4f5036ffa29a2f40caffd829ee0c
Guillermo German Speicher <guillermo.speicher@gmail.com>
2024-10-14 02:04:30 -0300

    feat(SubmitReusableButton): Button secondary text option added

--- app/src/main/java/com/example/ortbetp3grupo5parcial/ui/components/SubmitReusableButton.kt
+++ app/src/main/java/com/example/ortbetp3grupo5parcial/ui/components/SubmitReusableButton.kt
@@ -1,19 +1,39 @@
 package com.example.ortbetp3grupo5parcial.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.ortbetp3grupo5parcial.R
+import com.example.ortbetp3grupo5parcial.ui.theme.Gray10
+import com.example.ortbetp3grupo5parcial.ui.theme.Green40
+import com.example.ortbetp3grupo5parcial.ui.theme.Green60
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+import kotlin.coroutines.coroutineContext
 
 @Composable
 fun SubmitReusableButton(
@@ -21,24 +41,56 @@ fun SubmitReusableButton(
     buttonText: String,
     buttonWidth: Int = 364,
     buttonHeight: Int = 67,
-    buttonColor: Color = Color(0xFF34A853),
-    textColor: Color = Color.White
+    buttonColor: Color = Green40,
+    textColor: Color = Gray10,
+    secondaryText: String = ""
 ) {
     Button(
         onClick = onClick,
         modifier = Modifier
             .width(buttonWidth.dp)
-            .height(buttonHeight.dp),
+            .clip(RoundedCornerShape(19.dp))
+            .height(buttonHeight.dp)
+            .background(buttonColor),
         colors = ButtonDefaults.buttonColors(
             containerColor = buttonColor
         )
     ) {
-        Text(
-            text = buttonText,
-            color = textColor,
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Bold,
-            fontFamily = FontFamily(Font(R.font.poppins_medium))
-        )
+        Box(modifier = Modifier.fillMaxWidth()) {
+            Row(modifier = Modifier
+                .align(Alignment.Center),
+                horizontalArrangement = Arrangement.Center) {
+                Text(
+                    text = buttonText,
+                    color = textColor,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily(Font(R.font.poppins_medium)),
+                    textAlign = TextAlign.Center
+                )
+            }
+            // Texto secundario alineado a la derecha
+            if (secondaryText.isNotEmpty()) {
+                Row(modifier = Modifier
+                    .align(Alignment.CenterEnd)) {
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(4.dp))
+                            .background(Green60)
+                            .padding(4.dp)
+                            .zIndex(1f)
+                    ) {
+                        Text(
+                            text = secondaryText,
+                            color = textColor,
+                            fontSize = 12.sp,
+                            fontFamily = FontFamily(Font(R.font.poppins_medium))
+                        )
+                    }
+                }
+            }
+        }
+
+
     }
 }
--- app/src/main/java/com/example/ortbetp3grupo5parcial/ui/theme/Color.kt
+++ app/src/main/java/com/example/ortbetp3grupo5parcial/ui/theme/Color.kt
@@ -6,6 +6,9 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
+val Green60 = Color(0xFF489E67);
+
+
 val Gray40 = Color(0xFFB3B3B3)
 val Green40 = Color (0xFF53B175)
 val Pink40 = Color(0xFF7D5260)
@@ -13,3 +16,5 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 
 val Gray20 = Color(0xFFE2E2E2)
+
+val Gray10 = Color(0xFFFCFCFC)
