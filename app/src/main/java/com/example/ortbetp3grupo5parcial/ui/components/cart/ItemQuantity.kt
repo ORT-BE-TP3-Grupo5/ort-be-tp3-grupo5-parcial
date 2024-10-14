de0aa71009a2f46bdfd3b8d54d3e0be5263d80a4
Guillermo German Speicher <guillermo.speicher@gmail.com>
2024-10-13 22:09:42 -0300

    feat(CartItem): CartItem Ux finnished #22

---
+++ app/src/main/java/com/example/ortbetp3grupo5parcial/ui/components/cart/ItemQuantity.kt
@@ -0,0 +1,45 @@
+package com.example.ortbetp3grupo5parcial.ui.components.cart
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ortbetp3grupo5parcial.R
+import com.example.ortbetp3grupo5parcial.ui.components.QuantityButton
+
+
+@Composable
+fun ItemQuantity() {
+    var value by remember { mutableStateOf(1) }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.width(134.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        // Botón de decremento (-)
+        QuantityButton(onClick = { if (value > 1) value-- }, iconResource = R.drawable.ic_quantity_substract, invertedColors = true,
+            disabled = value == 1)
+
+        // Valor numérico entre los botones
+        Text(
+            text = value.toString(),
+            fontSize = 16.sp,
+            fontWeight =  FontWeight.Bold
+        )
+        // Botón de incremento (+)
+        QuantityButton(onClick = { value++ }, invertedColors = true)
+    }
+}
